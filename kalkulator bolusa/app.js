1a7b648ddc687015c98a5d4a576930f4ab785798
Hexon777 <119949819+Hexon777@users.noreply.github.com>
2023-01-17 11:16:42 +0100

    kalkulator bolusa
    niedokonczony

--- kalkulator bolusa/app.js
+++ kalkulator bolusa/app.js
@@ -1,19 +1,21 @@
-function Calculate_Bolus(operator)
-{
+function Calculate(operator){
+
     var DDI = document.getElementById('ddi').value;
-    var a = document.getElementById('a').value;
-    var b = document.getElementById('b').value;
-    var c = document.getElementById('c').value;
-    var d = document.getElementById('d').value;
+    var sugar = document.getElementById('cukier').value;
 
     switch(operator)
     {
         case 'Oblicz':
             var wrazliwosc = 1800 / parseFloat(DDI);
-
+            var insulin = parseFloat(sugar) - 100;
+            var insulina = parseFloat(insulin) / parseFloat(wrazliwosc);
             break;
     }
-    var wrazliwosc_zaokraglona = Math.ceil(wrazliwosc);
-    document.getElementById('wrazliwosc').value = wrazliwosc_zaokraglona;
 
+    var insulinaa = insulina * 100;
+    var insulina_zaokraglona = Math.round(insulinaa)/100;
+    
+    var wrazliwosc_zaokraglona = Math.ceil(wrazliwosc);
+    document.getElementById('insulina').value = insulina_zaokraglona;
+    document.getElementById('wrazliwosc').value = wrazliwosc_zaokraglona;
 }
