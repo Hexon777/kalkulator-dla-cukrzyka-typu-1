15f916d80d271618485b205fcc8c7fc21a1ef8a6
Hexon777 <119949819+Hexon777@users.noreply.github.com>
2023-01-18 16:57:44 +0100

    dokończenie wszystkiego
    dokończyłem  wszystko czego nie zrobiłem 👍😀🔚

--- kalkulator bolusa/app.js
+++ kalkulator bolusa/app.js
@@ -2,6 +2,8 @@ function Calculate(operator){
 
     var DDI = document.getElementById('ddi').value;
     var sugar = document.getElementById('cukier').value;
+    var wenglo = document.getElementById('wenglowodany').value;
+    var gh = document.getElementById('g/h').value;
 
     switch(operator)
     {
@@ -9,13 +11,20 @@ function Calculate(operator){
             var wrazliwosc = 1800 / parseFloat(DDI);
             var insulin = parseFloat(sugar) - 100;
             var insulina = parseFloat(insulin) / parseFloat(wrazliwosc);
+            var posilek = parseFloat(wenglo) / parseFloat(gh);
             break;
     }
 
     var insulinaa = insulina * 100;
     var insulina_zaokraglona = Math.round(insulinaa)/100;
+
+    var insulina_posilkowa = posilek * 100;
+    var posilek_zaokragonglony = Math.round(insulina_posilkowa)/100;
     
     var wrazliwosc_zaokraglona = Math.ceil(wrazliwosc);
+    var insulina_ogolna = parseFloat(insulina_zaokraglona) + parseFloat(posilek_zaokragonglony);
     document.getElementById('insulina').value = insulina_zaokraglona;
     document.getElementById('wrazliwosc').value = wrazliwosc_zaokraglona;
+    document.getElementById('posilek').value = posilek_zaokragonglony;
+    document.getElementById('insulina_ogolna').value = insulina_ogolna;
 }
